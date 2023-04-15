6505547ad69ad547f970f4ab946816642bd21b84
Robin <robin@rvanee.nl>
2023-04-15 13:03:10 +0200

    Arrays are done

---
+++ Code/Editor/Database/UI/ArrayEditor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine.UIElements;
+
+namespace WolfRPG.Core
+{
+	public class ArrayEditor: VisualElement
+	{
+		private GroupBox _propertyEditors;
+		public ArrayEditor(IRPGComponent component, PropertyInfo property, Type propertyType, ComponentEditor componentEditor)
+		{
+			var foldout = new Foldout();
+			foldout.text = property.Name;
+
+			var array = (Array) property.GetValue(component) ?? Array.CreateInstance(propertyType.GetElementType(), 0);
+
+			var buttonGroup = new GroupBox();
+			buttonGroup.AddToClassList("Horizontal");
+			
+			foldout.Add(buttonGroup);
+
+			var length = new Label(array.Length.ToString());
+
+			// Add new element
+			var addButton = new Button(() =>
+			{
+				Resize(ref array, array.Length + 1);
+				property.SetValue(component, array);
+				componentEditor.OnComponentUpdated?.Invoke();
+
+				length.text = array.Length.ToString();
+
+				RefreshPropertyEditors(propertyType, componentEditor, array);
+
+			});
+			addButton.text = "+";
+			
+			// Remove last element
+			var removeButton = new Button(() =>
+			{
+				Resize(ref array, array.Length - 1);
+				property.SetValue(component, array);
+				componentEditor.OnComponentUpdated?.Invoke();
+				
+				length.text = array.Length.ToString();
+				
+				RefreshPropertyEditors(propertyType, componentEditor, array);
+			});
+			removeButton.text = "-";
+
+			buttonGroup.Add(addButton);
+			buttonGroup.Add(removeButton);
+			buttonGroup.Add(length);
+
+			_propertyEditors = new();
+			foldout.Add(_propertyEditors);
+
+			RefreshPropertyEditors(propertyType, componentEditor, array);
+			
+			Add(foldout);
+		}
+
+		private void RefreshPropertyEditors(Type propertyType, ComponentEditor componentEditor, Array array)
+		{
+			_propertyEditors.Clear();
+			
+			for (var i = 0; i < array.Length; i++)
+			{
+				var index = i;
+				var propertyEditor =
+					new PropertyEditor(propertyType.GetElementType(), array, index, componentEditor);
+				_propertyEditors.Add(propertyEditor);
+			}
+		}
+
+		private static void Resize(ref Array array, int newSize) {        
+			var elementType = array.GetType().GetElementType();
+			var newArray = Array.CreateInstance(elementType, newSize);
+			Array.Copy(array, newArray, Math.Min(array.Length, newArray.Length));
+			array = newArray;
+		}
+	}
+}
--- Code/Editor/Database/UI/ComponentEditor.cs
+++ Code/Editor/Database/UI/ComponentEditor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using UnityEngine.UIElements;
 
 namespace WolfRPG.Core
@@ -18,36 +17,7 @@ namespace WolfRPG.Core
 				var propertyType = property.PropertyType;
 				if (propertyType.IsArray)
 				{
-					var foldout = new Foldout();
-					foldout.text = property.Name;
-					
-					var array = (Array) property.GetValue(component) ?? Array.CreateInstance(propertyType.GetElementType(), 0);
-					
-					var button = new Button(() =>
-					{
-						Resize(ref array, array.Length + 1);
-						property.SetValue(component, array);
-						OnComponentUpdated?.Invoke();
-
-						var propertyEditor =
-							new PropertyEditor(propertyType.GetElementType(), array, array.Length - 1, this);
-						foldout.Add(propertyEditor);
-					});
-					button.text = "+";
-					
-					foldout.Add(button);
-
-					for (var i = 0; i < array.Length; i++)
-					{
-						var index = i;
-						var propertyEditor =
-							new PropertyEditor(propertyType.GetElementType(), array, index, this);
-						foldout.Add(propertyEditor);
-					}
-
-					
-					
-					Add(foldout);
+					Add(new ArrayEditor(component, property, propertyType, this));
 				}
 				else
 				{
@@ -55,13 +25,6 @@ namespace WolfRPG.Core
 					Add(editor);
 				}
 			}
-		}
-		
-		private static void Resize(ref Array array, int newSize) {        
-			var elementType = array.GetType().GetElementType();
-			var newArray = Array.CreateInstance(elementType, newSize);
-			Array.Copy(array, newArray, Math.Min(array.Length, newArray.Length));
-			array = newArray;
 		}
 	}
 }
